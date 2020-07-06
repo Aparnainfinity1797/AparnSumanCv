f894b492df32bcf2733f706a1073823bc77792a3
Aparna <aparna.suman@infinitylabs.in>
2020-07-06 16:18:28 +0530

    changes

--- src/Intrestmain.js
+++ src/Intrestmain.js
@@ -8,7 +8,7 @@ return (
 <section className="resume-section" id="interests">
                 <div className="resume-section-content">
                     <h2 className="mb-5">Interests</h2>
-                    <div className="alig"> test:{this.props.simpleReducer && this.props.simpleReducer.result}</div>
+      
                     <p>Apart from being a web developer, I enjoy most of the time to deeloped new idea in front end animation with refrence from google</p>
                     <p className="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p>
                 </div>
